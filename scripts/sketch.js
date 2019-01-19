1c314ae276da5d65a01b2bb0e015f42bdf0bfa81
Marc Diaz <marcgabe15@gmail.com>
2019-01-19 17:35:07 -0500

    MARK: added text nodes

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,5 +1,50 @@
 
+function initMap() 
+{
+  var options = 
+  {
+    zoom: 15,
+    center: {lat: 29.6480, lng: -82.3440},
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  }
 
+  // New Map
+  var mainMap = new google.maps.Map(document.getElementById('map'), options);
+
+  google.maps.event.addListener(mainMap, 'rightclick', function(event) {
+    
+    addMarker({coords:event.latLng});
+  
+  });
+
+  // Add Marker Function
+  function addMarker(property)
+  {
+    var marker = new google.maps.Marker({
+
+      position: property.coords,
+      map: mainMap
+    
+    });
+
+    var printScreen = '<h4>What is the problem: </h4> <input id = "problem"></input>' + 
+    '<br/>' + '<h4> What is the building: </h4> <input id = "problem"></input>' + '<br/>'
+    + '<button id = "Submit">submit</button>';
+
+    // Check content to see if already edited
+    var infoWindow = new google.maps.InfoWindow({
+
+      content: printScreen
+    
+    });
+
+    marker.addListener('click', function(){
+
+      infoWindow.open(mainMap, marker);
+    
+    });
+  }
+}
 
 
 //AIzaSyBpkcR8rXn5QMzjSAZ5Nyyl733P1DWCr3g
